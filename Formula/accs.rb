94fb0ebc74009ebd7b413e352e4a473ed8fc6071
Shota Kashihara <ShotaKashihara@users.noreply.github.com>
2021-02-07 15:06:17 +0000

    accs 1.0.4

--- Formula/accs.rb
+++ Formula/accs.rb
@@ -1,8 +1,8 @@
 class Accs < Formula
   desc "AtCoder command line tools for Swift (accs)"
   homepage "https://github.com/ShotaKashihara/atcoder-cli-swift"
-  url "https://github.com/ShotaKashihara/atcoder-cli-swift/archive/1.0.3.tar.gz"
-  sha256 "2837877bbf81ba52fd110e02cbed2f847e6e8679fbce9a816319d9e71115a634"
+  url "https://github.com/ShotaKashihara/atcoder-cli-swift/archive/1.0.4.tar.gz"
+  sha256 "0a014c0a14cbb25caf3c9b46e318a10ad4154054eb0d03e8bda591c1ec2b9798"
 
   def install
     system "make", "install", "PREFIX=#{prefix}"
